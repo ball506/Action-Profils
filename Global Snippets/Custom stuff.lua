7bf4177e00481cb4268d62509b30552f75164b21
Ultrasn0w <skunkyfok@gmail.com>
2019-09-08 15:40:54 +0200

    [Global Snippet] Add GetByRangeDoTsToRefresh

--- Global Snippets/Custom stuff.lua
+++ Global Snippets/Custom stuff.lua
@@ -2,7 +2,7 @@ local TMW = TMW
 local CNDT = TMW.CNDT
 local Env = CNDT.Env
 local Action = Action
-
+local MultiUnits = Action.MultiUnits
 local HL = HeroLib;
 local Cache = HeroCache;
 local Unit = HL.Unit;
@@ -87,6 +87,8 @@ if currentClass == "DEMONHUNTER" then
     Action.Print("Automatically loaded profile : [Taste]Action - Demon Hunter")
 end
 
+-- Movement checker
+-- @return number
 local movedTimer = 0
 function Unit:MovingFor()
     if not self:IsMoving() then
@@ -106,9 +108,9 @@ function Player:InBattlegrounds()
 end
 
 function Player:InPvP()
-    return self:InArena() or self:InBattlegrounds()
+    return select(2, IsInInstance()) == "pvp" or select(2, IsInInstance()) == "arena"
 end
-
+ 
 function Player:InDungeon()
     return select(2, IsInInstance()) == "party" or C_Map.GetBestMapForUnit("Player") == 480
 end
@@ -118,3 +120,25 @@ function Player:InRaid()
 end
 
 
+function Action.MultiUnits.GetByRangeDoTsToRefresh(self, range, count, deBuffs, refreshTime, upTTD)
+	-- @return number
+	-- @usage A.MultiUnits:GetByRangeDoTsToRefresh(@number, @number, @table or @number, @number, @number)
+	-- deBuffs is required, refreshTime too, rest options are optimal
+	local total = 0
+	local nameplates = self:GetActiveUnitPlates()
+	
+	if nameplates then 
+		for unitID in pairs(nameplates) do 
+			if Action.Unit(unitID):CombatTime() > 0 and (not range or Action.Unit(unitID):CanInterract(range)) and (not upTTD or Action.Unit(unitID):TimeToDie() >= upTTD) and Action.Unit(unitID):HasDeBuffs(deBuffs, true) <= refreshTime then 
+				total = total + 1
+			end 
+			
+			if count and total >= count then 
+				break 
+			end 
+		end 
+	end 
+	
+	return total 
+end 
+Action.MultiUnits.GetByRangeDoTsToRefresh = Action.MakeFunctionCachedDynamic(Action.MultiUnits.GetByRangeDoTsToRefresh)
