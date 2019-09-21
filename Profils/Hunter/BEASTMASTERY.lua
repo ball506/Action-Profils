9034c634e3ba724a63ba713da7ccb934a37103fa
Ultrasn0w <skunkyfok@gmail.com>
2019-09-21 12:04:04 +0200

    [Hunter] BeastMastery Guardian of Azeroth fix

--- Profils/Hunter/BEASTMASTERY.lua
+++ Profils/Hunter/BEASTMASTERY.lua
@@ -435,7 +435,7 @@ local function APL()
             if HR.Cast(S.WorldveinResonance) then return "worldvein_resonance"; end
         end
         -- guardian_of_azeroth,if=cooldown.aspect_of_the_wild.remains<10|target.time_to_die>cooldown+duration|target.time_to_die<30
-        if S.GuardianofAzeroth:IsCastableP() and not ShouldStop and Action.GetToggle(1, "HeartOfAzeroth") and (S.AspectoftheWild:CooldownRemainsP() < 10 or Target:TimeToDie() > S.GuardianofAzeroth:Cooldown() + S.GuardianofAzeroth:BaseDuration() or Target:TimeToDie() < 30) then
+         if S.GuardianofAzeroth:IsCastableP() and (S.AspectoftheWild:CooldownRemainsP() < 10 or Target:TimeToDie() > 180 + S.GuardianofAzeroth:BaseDuration() or Target:TimeToDie() < 30) then
             if HR.Cast(S.GuardianofAzeroth) then return "guardian_of_azeroth"; end
         end
         -- ripple_in_space
