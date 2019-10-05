5e0bab2ed2e69111bec2adf5b69319c9514ad2bc
Ultrasn0w <skunkyfok@gmail.com>
2019-10-05 10:48:20 +0200

    [ProfileUI] Preparing 1.19 update

--- Profils/Retail/Demon Hunter/ProfileUI.lua
+++ Profils/Retail/Demon Hunter/ProfileUI.lua
@@ -7,7 +7,7 @@ local Env = CNDT.Env
 local A = Action
 A.Data.ProfileEnabled[TMW.db:GetCurrentProfile()] = true
 A.Data.ProfileUI = {      
-    DateTime = "v1.18 (28.09.2019)",
+    DateTime = "v1.19 (05.10.2019)",
     -- Class settings
     [2] = {        
         [ACTION_CONST_DEMONHUNTER_HAVOC] = {          
