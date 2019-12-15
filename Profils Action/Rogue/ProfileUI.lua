50bfcfece35132b52b9f9233e07bc0fd08f6b930
Ultrasn0w <skunkyfok@gmail.com>
2019-12-15 15:59:16 +0100

    [Rogue] Update ProfileUI for Outlaw

--- Profils Action/Rogue/ProfileUI.lua
+++ Profils Action/Rogue/ProfileUI.lua
@@ -19,7 +19,7 @@ local HeroUnit                                      = HL.Unit
 
 A.Data.ProfileEnabled[TMW.db:GetCurrentProfile()] = true
 A.Data.ProfileUI = {    
-    DateTime = "TEST v2.0.3 (14.12.2019)",
+    DateTime = "v2.0.4 (15.12.2019)",
     -- Class settings
     [2] = {        
         [ACTION_CONST_ROGUE_OUTLAW] = {       
