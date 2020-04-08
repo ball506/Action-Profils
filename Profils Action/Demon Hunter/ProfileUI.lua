a70ce34b171cab4b6efc8dc1d54d52a4ce0220cb
Ultrasn0w <skunkyfok@gmail.com>
2020-04-08 12:20:56 +0200

    [Demon Hunter] Havoc ProfileUI DateTime

--- Profils Action/Demon Hunter/ProfileUI.lua
+++ Profils Action/Demon Hunter/ProfileUI.lua
@@ -24,7 +24,7 @@ local select, setmetatable							= select, setmetatable
 
 A.Data.ProfileEnabled[TMW.db:GetCurrentProfile()] = true
 A.Data.ProfileUI = {      
-    DateTime = "v4.1.6 (02.04.2020)",
+    DateTime = "v4.1.8 (08.04.2020)",
     -- Class settings
     [2] = {        
         [ACTION_CONST_DEMONHUNTER_HAVOC] = {   
