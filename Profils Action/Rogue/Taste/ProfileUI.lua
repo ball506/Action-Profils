8afc713b25c5d40bc57421b6c73fa7e4aac29944
Ultrasn0w <skunkyfok@gmail.com>
2020-05-23 14:07:58 +0200

    [Rogue] ProfileUI update DateTime

--- Profils Action/Rogue/Taste/ProfileUI.lua
+++ Profils Action/Rogue/Taste/ProfileUI.lua
@@ -25,7 +25,7 @@ local select, setmetatable							= select, setmetatable
 
 A.Data.ProfileEnabled[TMW.db:GetCurrentProfile()] = true
 A.Data.ProfileUI = {    
-    DateTime = "v4.0.4 (22.05.2020)",
+    DateTime = "v4.0.5 (23.05.2020)",
     -- Class settings
     [2] = {        
         [ACTION_CONST_ROGUE_OUTLAW] = {  
