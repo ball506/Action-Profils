eea985009112e460b20be3a3d765e0a3fcbbed0f
Ultrasn0w <skunkyfok@gmail.com>
2020-07-22 18:59:40 +0200

    [Warlock] ProfileUI DateTime

--- Profils Action/Warlock/ProfileUI.lua
+++ Profils Action/Warlock/ProfileUI.lua
@@ -25,7 +25,7 @@ local select, setmetatable							= select, setmetatable
 
 A.Data.ProfileEnabled[TMW.db:GetCurrentProfile()] = true
 A.Data.ProfileUI = {    
-    DateTime = "v4.1.3 (22.05.2020)",
+    DateTime = "v4.1.4 (22.07.2020)",
     -- Class settings
     [2] = {        
         [ACTION_CONST_WARLOCK_AFFLICTION] = {  
