ffe21a373b49d56e2620807c60e13c3ec22a9627
Ultrasn0w <skunkyfok@gmail.com>
2020-07-22 14:23:11 +0200

    [Shaman] ProfileUI DateTime

--- Profils Action/Shaman/ProfileUI.lua
+++ Profils Action/Shaman/ProfileUI.lua
@@ -24,7 +24,7 @@ local select, setmetatable							= select, setmetatable
 
 A.Data.ProfileEnabled[TMW.db:GetCurrentProfile()] = true
 A.Data.ProfileUI = {    
-    DateTime = "v4.2.2 (21.07.2020)",
+    DateTime = "v4.2.3 (22.07.2020)",
     -- Class settings
     [2] = {        
         [ACTION_CONST_SHAMAN_ENCHANCEMENT] = {
