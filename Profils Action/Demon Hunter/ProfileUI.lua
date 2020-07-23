d857661f0d07e6cd9797af9d476f639e2e3a6c37
Ultrasn0w <skunkyfok@gmail.com>
2020-07-24 00:18:36 +0200

    [Demon Hunter] ProfileUI DateTime

--- Profils Action/Demon Hunter/ProfileUI.lua
+++ Profils Action/Demon Hunter/ProfileUI.lua
@@ -22,9 +22,9 @@ local InstanceInfo									= A.InstanceInfo
 local TR                                            = Action.TasteRotation
 local select, setmetatable							= select, setmetatable
 
-A.Data.ProfileEnabled[TMW.db:GetCurrentProfile()] = true
+A.Data.ProfileEnabled[Action.CurrentProfile] = true
 A.Data.ProfileUI = {      
-    DateTime = "v4.3.2 (21.07.2020)",
+    DateTime = "v4.3.3 (24.07.2020)",
     -- Class settings
     [2] = {        
         [ACTION_CONST_DEMONHUNTER_HAVOC] = {   
